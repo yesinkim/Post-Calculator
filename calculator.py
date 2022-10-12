f97ebf8232d182ecccc5ac2c834beffe3244081a
yesin kim <yeshinkim@yeshinui-MacBookPro.local>
2022-10-12 18:17:32 +0900

    Add Feedback

--- calculator.py
+++ calculator.py
@@ -6,7 +6,7 @@
 ㅠㅠㅠㅠㅠㅠㅠ
 """
 
-# value class Token 
+
 # 더 여러가지 일을 해야 한다면?
 
 import re
@@ -24,27 +24,37 @@ class LexicalAnalyzer:
     def __init__(self):
         # self.expression = "2.7 - 3 * (1+2)"        # temp test expression
         # self.expression = "2+2 + 4"
-        self.expression = " 02 + 4 + 5"
-        # self.expression = input("Enter an expression: ").replace(" ", "")
+        # self.expression = " 02 + 4 + 5"
+        self.expression = input("Enter an expression: ").replace(" ", "")
         self.check_expression()
 
     def check_expression(self):
         expression = self.expression.replace(" ", "")
+        print(expression)
         pattern = re.compile("[^\d.\/\+\*\-\(\)]")
         pattern2 = re.compile(r"([\+\-\*\/])\1+")
 
         if expression == "":
-            raise ExpressionError("Empty expression")
+            raise ExpressionError("Empty expression")       # error 세부화
+
         elif pattern.search(expression):
             raise ExpressionError("Unexpected character")
+
         elif pattern2.search(expression):
             raise ExpressionError("Overlap operator")
+
         elif expression.startswith(("*", "/", "+", "-", ")")) or expression.endswith(("*", "/", "+", "-", "(")):
             raise ExpressionError("Wrong expression")
+
         elif expression.count("(") != expression.count(")") or expression.find("(") > expression.find(")"):       # ") 1 + 2 (" 같은 경우 걸러지지 않음
             raise ExpressionError("Parentheses error")
 
     def to_postfix(self):
+        """_summary_
+
+        Returns:
+            _type_: _description_
+        """
         stack = []
         postfix = []
         expression = re.findall(r"[0-9.]+|[\+\-\*\/\(\)]", self.expression)
@@ -79,18 +89,25 @@ class LexicalAnalyzer:
 
 def Calculate(expression):
     stack = []
+
     for token in expression:
         if token.value == '+':
             stack.append(Operand(stack.pop()+stack.pop()))
+
         elif token.value == '-':
             stack.append(Operand(stack.pop()-stack.pop()))
+
         elif token.value == '*':
             stack.append(Operand(stack.pop()*stack.pop()))
+
         elif token.value == '/':
+            # 0으로 나눌 수 없음. -> Zero Division Error 발생 예외처리
             rv = stack.pop()
             stack.append(Operand(stack.pop()/rv))
+
         else:
             stack.append(token)
+
     return stack.pop()
 
 
@@ -99,7 +116,10 @@ def Calculate(expression):
 
 
 
-
+# test code 작성하기
+# input spec에서 정의한 것들을 테스트케이스로 보여줘야 함
+# input spec에 정의한 것들은 모두 실행할 수 있어야 하고
+# test case 를 꼼꼼하게 작성!! => 예외처리하는 것에 대한 통찰을 기를 수 있음
 if __name__ == "__main__":
     calc = LexicalAnalyzer()
     post_expression = calc.to_postfix()
--- tokens.py
+++ tokens.py
@@ -13,22 +13,19 @@ class Base():
         pass
 
 
-
 class Operator(Base):
     def __init__(self, value):
         super().__init__(value)
+        # 여기서 is_valid를 확인해야 함
     
     @classmethod
     def is_valid(cls, value):
         return value in "+-*/"
     
-    @classmethod        # class method는 독립된 함수처럼 작동함
+    @classmethod        # classmethod는 독립된 함수처럼 작동함
     def check_priority(cls, value):
         priority = {'+':1, '-':1, '*':2, '/':2}
         return priority[str(value)]
-
-
-
 
 
 class Operand(Base):
